92d698504ce67077691e4159cd7e4406318b11ab
Juan Miguel <juanmi.marisanchez@gmail.com>
2018-01-22 15:06:52 +0100

    Jump

--- js/game.js
+++ js/game.js
@@ -1,60 +1,67 @@
 var myGamePiece;
 
 function startGame() {
-    myGamePiece = new component(30, 30, "imgs/pj.png", 10, 120, "image");
+    myGamePiece = new component(30, 30, "red", 30, 175);
     myGameArea.start();
 }
+
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
         this.canvas.width = 480;
         this.canvas.height = 270;
-        this.canvas.style.cursor = "none"; //hide the original cursor
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 1);
-        window.addEventListener('mousemove', function (e) {
-            myGameArea.x = e.pageX;
-            myGameArea.y = e.pageY;
-        })
+        this.interval = setInterval(updateGameArea, 20);
     },
-    clear : function(){
+    stop : function() {
+        clearInterval(this.interval);
+    },
+    clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
+
 function component(width, height, color, x, y, type) {
     this.type = type;
-    if (type == "image") {
-        this.image = new Image();
-        this.image.src = color;
-    }
     this.width = width;
     this.height = height;
-    this.speedX = 0;
-    this.speedY = 0;
     this.x = x;
     this.y = y;
+    this.speedX = 0;
+    this.gravity = 4;
     this.update = function() {
         ctx = myGameArea.context;
-        if (type == "image") {
-            ctx.drawImage(this.image,
-                this.x,
-                this.y,
-                this.width, this.height);
-        } else {
-            ctx.fillStyle = color;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    };
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.gravity;
+        this.hitBottom();
+    };
+    this.hitBottom = function() {
+        var rockbottom = myGameArea.canvas.height - this.height;
+        if (this.y > rockbottom) {
+            this.y = rockbottom;
         }
+
+    this.jump = function (){
+            alert(this.y);
+        if (this.y<=240){
+            while (this.y<=200){
+                this.y -= this.gravity;
+            }
+        }
+        else{this.y += this.gravity;}
+        }
+
     }
 }
 
 function updateGameArea() {
     myGameArea.clear();
-    if (myGameArea.x && myGameArea.y) {
-        myGamePiece.x = myGameArea.x;
-        myGamePiece.y = myGameArea.y;
-        checkKey
-    }
+    myGamePiece.newPos();
     myGamePiece.update();
 }
 
@@ -63,13 +70,15 @@ function updateGameArea() {
 //Refactorizar
 document.onkeydown = checkKey;
 document.onkeyup = checkKey2;
+//document.onkeyup = accelerate(0.1);
 
 function checkKey(e) {
 
     e = e || window.event;
 
     if (e.keyCode == '32') {
-            pictureChange();
+        pictureChange();
+        myGamePiece.jump();
     }
 }
 
